150a09eccad5b070795123a9515b4b7ee4ed0273
brianneking86 <brianneking86@gmail.com>
2014-12-27 14:19:26 -0500

    updated

--- Gemfile
+++ Gemfile
@@ -21,6 +21,8 @@ gem 'sass'
 
 gem 'bower-rails'
 
+gem 'figaro'
+
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 2.0'
 # bundle exec rake doc:rails generates the API under doc/api.
--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,27 +1,52 @@
-angular.module('codechallenge', []).controller('MainCtrl', [
-  '$scope',
-  function($scope){
-    $scope.test = 'Hello world!';
-    $scope.posts = [
-      {title: 'post 1', upvotes: 5},
-      {title: 'post 2', upvotes: 2},
-      {title: 'post 3', upvotes: 15},
-      {title: 'post 4', upvotes: 9},
-      {title: 'post 5', upvotes: 4}
-    ];
-    $scope.addPost = function(){
-      if(!$scope.title || $scope.title === '') { return; }
-      $scope.posts.push({
-        title: $scope.title,
-        link: $scope.link,
-        upvotes: 0
-      });
-      $scope.title = '';
-      $scope.link = '';
+angular.module('codechallenge', []).controller('MainCtrl', ['$scope', function($scope){
+    $scope.greeting = "Hi, add and view your files here";
+    // var bucket = new AWS.S3({params: {Bucket: 'yh.interview'}});
+    $scope.addFile = function(){
+      debugger;
+
+      // bucket.putObject({
+      //   Key: "brianneking/trialfile.txt", // this is basically the filename -- replace YOURNAME with your first and last name, lowercase, no spaces
+      //   ACL: "public-read", // must include this exactly as is, this is what allows you to get the uploaded file from your browser
+      //   Body: "trial file" // this is the content of the file
+      //   }, function(err, data) { // optional -- this is the callback that is executed when the operation is complete; 
+      //                            // highly recommended -- it will be nice to know when it has worked and when it has failed;
+      //                            // the user will probably want to know, too 
+      //     if (err) {
+      //       console.log(err);
+      //     }
+      //     if (data) {
+      //       console.log(data);
+      //     }
+      //   }
+      // )
     };
-    $scope.incrementUpvotes = function(post) {
-      post.upvotes += 1;
-    };
+    
+  //   $scope.listFile = function(){
+  //     bucket.listObjects({
+  //       Prefix: "brianneking" // again replace with your first and last name, lower case, no spaces; must include this key
+  //     }, function(err, data) {
+  //       if (err) {
+  //         console.log(err)
+  //       }
+  //       if (data) {
+  //         console.log(data) // you'll want to do something more interesting than console.log with the data
+  //                           // data will be a javascript object that looks something like this:
+  //                           // {
+  //                           //   CommonPrefixes: [],
+  //                           //   Contents: [
+  //                           //    {ETag: "number", Key: "first_filename", LastModified: aDate, Size: 200, StorageClass: "STANDARD"},
+  //                           //    {ETag: "anotherNumber", Key: "second_filename", LastModified: anotherDate, Size: 500, StorageClass: "STANDARD"},
+  //                           //   ],
+  //                           //   IsTruncated: false,
+  //                           //   Marker: "",
+  //                           //   MaxKeys: 1000, 
+  //                           //   Name: "yh.interview",
+  //                           //   Prefix: "YOURNAME"
+  //                           // }
+  //                           // you're probably only going to be interested in Contents, and then the Key attribute from the objects in that array
+  //                           // data.Contents[0].Key will get you "first_filename", the name of the first file in your bucket
+  //       }
+  //   });
+  // };
   }
 ]);
-
